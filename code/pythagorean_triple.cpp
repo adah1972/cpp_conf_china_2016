4fbf787178c65212bc0f85bac8bf79ef9d061688
Wu Yongwei <wuyongwei@gmail.com>
2016-10-29 20:30:33 +0800

    Simplify code with view::ints; make code compile under C++14.

--- code/pythagorean_triple.cpp
+++ code/pythagorean_triple.cpp
@@ -5,20 +5,13 @@ using namespace ranges;
 
 int main()
 {
-    // Lazy ranges for generating integer sequences
-    auto const intsFrom = view::iota;
-    auto const ints = [=](int i, int j)
-        {
-            return view::take(intsFrom(i), j - i + 1);
-        };
-
     // Define an infinite range of all the Pythagorean
     // triples:
-    auto triples = view::for_each(intsFrom(1), [=](int z)
+    auto triples = view::for_each(view::ints(1), [=](int z)
         {
-            return view::for_each(ints(1, z), [=](int x)
+            return view::for_each(view::ints(1, z), [=](int x)
             {
-                return view::for_each(ints(x, z), [=](int y)
+                return view::for_each(view::ints(x, z), [=](int y)
                 {
                     return yield_if(x*x + y*y == z*z,
                         std::make_tuple(x, y, z));
@@ -27,7 +20,7 @@ int main()
         });
 
     // Display the first 10 triples
-    for (auto triple : triples | view::take(10)) {
+    for (auto triple : triples | view::take(10) | view::bounded) {
         std::cout << '('
             << std::get<0>(triple) << ','
             << std::get<1>(triple) << ','
